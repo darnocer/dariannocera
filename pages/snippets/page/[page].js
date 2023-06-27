642482e75da25ab533d77c0f4e4a77c3072d29dc
Darian Nocera <darnocer@gmail.com>
2023-06-26 21:13:03 -0600

    resolve getStaticPaths() build error in snippets

--- pages/snippets/page/[page].js
+++ pages/snippets/page/[page].js
@@ -1,20 +1,44 @@
-import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
-import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
-import formatDate from '@/lib/utils/formatDate'
+import { POSTS_PER_PAGE } from '../../snippets'
 import TileLayout from '@/layouts/TileLayout'
 
-import NewsletterForm from '@/components/NewsletterForm'
+export async function getStaticPaths() {
+  const totalPosts = await getAllFilesFrontMatter('blog')
+  const totalPages = Math.ceil(totalPosts.length / POSTS_PER_PAGE)
+  const paths = Array.from({ length: totalPages }, (_, i) => ({
+    params: { page: (i + 1).toString() },
+  }))
 
-const MAX_DISPLAY = 5
+  return {
+    paths,
+    fallback: false,
+  }
+}
 
-export async function getStaticProps() {
-  const posts = await getAllFilesFrontMatter('snippets')
-  console.log('posts', posts)
+export async function getStaticProps(context) {
+  const {
+    params: { page },
+  } = context
+  const posts = await getAllFilesFrontMatter('blog')
+  const pageNumber = parseInt(page)
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+  }
 
-  return { props: { posts } }
+  return {
+    props: {
+      posts,
+      initialDisplayPosts,
+      pagination,
+    },
+  }
 }
 
 export default function Snippets({ posts, initialDisplayPosts, pagination }) {
